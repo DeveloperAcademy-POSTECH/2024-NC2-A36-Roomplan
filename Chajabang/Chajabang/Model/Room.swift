009670d908bd51af4743939ec1fb1fe1f38fdff1
jiyoungsophia <sophiathedev@gmail.com>
2024-06-19 15:50:22 +0900

    Chore: add model extension

--- Chajabang/Chajabang/Model/Room.swift
+++ Chajabang/Chajabang/Model/Room.swift
@@ -10,6 +10,20 @@ import RoomPlan
 
 struct RoomObject: Hashable, Identifiable {
     let id = UUID()
+    var object: CapturedRoom.Object
     var category: CapturedRoom.Object.Category
     var memos: [String] = []
 }
+
+extension CapturedRoom.Object: Hashable, Equatable {
+    public static func == (lhs: CapturedRoom.Object, rhs: CapturedRoom.Object) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+}
+
+extension CapturedRoom.Object.Category: Hashable, Equatable {}
+
+
